469a4bafcbc8997ea44f87206e169e5363233858
Lukas Neverauskis <lukas.neverauskis@gmail.com>
2025-01-17 23:02:34 +0200

    improve install.sh

--- .bin/install.sh
+++ .bin/install.sh
@@ -1,14 +1,64 @@
-git clone --bare https://github.com/lukasnee/dotfiles.git $HOME/.dotfiles
-function dotfiles {
-   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+echo "Installing lukasnee/dotfiles..."
+dotfiles_alias="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+# define a function dotfiles
+dotfiles() {
+    $dotfiles_alias $@
 }
-mkdir -p .dotfiles-backup
-dotfiles checkout
+expected_dotfiles_remote="https://github.com/lukasnee/dotfiles.git"
+if [ -d "$HOME/.dotfiles" ]; then
+    if [ "$(dotfiles remote get-url origin)" = $expected_dotfiles_remote ]; then
+        echo "repository '$HOME/.dotfiles' already exists."
+    else
+        echo "repository '$HOME/.dotfiles' already exists, but is not the expected remote."
+        exit 1
+    fi
+else
+    echo "Cloning dotfiles repository..."
+    git clone --bare $expected_dotfiles_remote $HOME/.dotfiles
+fi
+
+dotfiles config --local status.showUntrackedFiles no
+
+dotfiles checkout > /dev/null 2>&1
 if [ $? = 0 ]; then
-  echo "Checked out dotfiles.";
-  else
-    echo "Backing up pre-existing dot files.";
-    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+    echo "Checked out dotfiles.";
+else
+    conflicting_files=$(dotfiles checkout 2>&1 | egrep "^\s+" | awk {'print $1'})
+    echo "Conflicting files found: $(echo $conflicting_files | tr '\n' ' ')"
+    mkdir -p .dotfiles-backup
+    echo "Backing up conflicting files to .dotfiles-backup/"
+    for file in $conflicting_files; do
+        cmd="mv $file .dotfiles-backup/"
+        echo $cmd
+        $cmd
+    done
 fi;
-dotfiles checkout
-dotfiles config status.showUntrackedFiles no
+dotfiles checkout > /dev/null 2>&1
+if [ ! $? = 0 ]; then
+    echo "Error: Could not checkout dotfiles."
+    exit 1
+fi;
+
+dotfiles_alias_cmd="alias dotfiles='$dotfiles_alias'"
+if [ -n $BASH_VERSION ]; then
+    if grep -q "$dotfiles_alias_cmd" $HOME/.bashrc; then
+        echo "'dotfiles' alias already exists in '$HOME/.bashrc'"
+    else
+        echo "Creating 'dotfiles' alias in '$HOME/.bashrc'"
+        echo $dotfiles_alias_cmd >> $HOME/.bashrc
+    fi;
+    cmd="source $HOME/.bashrc"
+elif [ -n $ZSH_VERSION ]; then
+    if grep -q "$dotfiles_alias_cmd" $HOME/.zshrc; then
+        echo "'dotfiles' alias already exists in '$HOME/.zshrc'"
+    else
+        echo "Creating 'dotfiles' alias in '$HOME/.zshrc'"
+        echo $dotfiles_alias_cmd >> $HOME/.zshrc
+    fi;
+    cmd="source $HOME/.zshrc"
+else
+    echo "Could not determine shell. Please add the following alias to your shell configuration:"
+    echo $dotfiles_alias_cmd
+    exit 1
+fi
+echo "Please source your shell configuration using '$cmd' to apply the changes to your current shell."
--- README.md
+++ README.md
@@ -1,36 +1,19 @@
 # Dotfiles for [@lukasnee](https://github.com/lukasnee)
 
-Repo logistics based on this neat [guide](https://www.atlassian.com/git/tutorials/dotfiles).
-
-## Requirements
+## Prerequisites
 
 - Git
 - Curl
+- Zsh
+- Oh My Zsh
 
 ## Install
 
-Install dotfiles tracking in your $HOME by running:
+For bash and zsh:
 
-    git init --bare $HOME/.dotfiles
-    echo "alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'" >> $HOME/.bashrc
-    source ~/.bashrc
-    dotfiles config --local status.showUntrackedFiles no
-    curl -Lks https://raw.githubusercontent.com/lukasnee/dotfiles/main/.bin/install.sh | /bin/bash
-
-> **NOTE:** During installation you created alias `dotfiles` - use it instead of `git` in your `$HOME` directory because it's not a normal repository.
-
-
-# Useful
-
-## Shell Tricks
-Generate a random data file of given size in bytes  
-```shell
-$ head -c 1024 /dev/urandom > 1KB.dat
+```zsh
+curl -Lks https://raw.githubusercontent.com/lukasnee/dotfiles/main/.bin/install.sh | /bin/bash
 ```
 
-## Regex Tricks
-
-## Match block between HEAD and TAIL that in between contains a PATTERN
-```regex
-(?m)HEAD(?:(?!HEAD)[\s\S])+(PATTERN)[\s\S]+?(?=TAIL)
-```
+> [!IMPORTANT]
+> Use git alias `dotfiles` instead of `git` to interact with this repository.
